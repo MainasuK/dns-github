8d18fa399d59c6349c7c9f6661968da07b247bc9
CMK <cirno.mainasuk@gmail.com>
2025-03-18 17:20:16 +0800

    chore: add the particle.network hostname

--- scripts/bin.mjs
+++ scripts/bin.mjs
@@ -2,113 +2,134 @@
 
 /* global $, fs, path */
 
-const writer = async arr => {
-  let temp = {}
-  const list = []
+const writer = async (arr) => {
+  let temp = {};
+  const list = [];
 
   for (const url of arr) {
     if (/^\.*\//.test(url)) {
-      list.push(temp = {
-        file: path.resolve(url),
-        contents: [],
-        content: '',
-      })
-      continue
+      list.push(
+        (temp = {
+          file: path.resolve(url),
+          contents: [],
+          content: "",
+        })
+      );
+      continue;
     }
 
     if (temp.contents) {
-      temp.contents.push(url)
+      temp.contents.push(url);
     }
   }
 
   for (const temp of list) {
-    temp.content = await updater(temp.contents)
+    temp.content = await updater(temp.contents);
   }
 
   for (const temp of list) {
-    fs.ensureFileSync(temp.file)
-    fs.writeFile(temp.file, temp.content)
+    fs.ensureFileSync(temp.file);
+    fs.writeFile(temp.file, temp.content);
   }
-}
+};
 
-const updater = async arr => {
-  const templates = []
-  const datetime = new Date().toUTCString()
+const updater = async (arr) => {
+  const templates = [];
+  const datetime = new Date().toUTCString();
 
-  templates.push(`# Update: ${datetime}\n`)
+  templates.push(`# Update: ${datetime}\n`);
 
   for (const url of arr) {
     if (url) {
-      const ip = await nslookup(url)
-      const dns = `${ip}\t\t${url}`
+      const ip = await nslookup(url);
+      const dns = `${ip}\t\t${url}`;
 
       if (ip) {
-        templates.push(dns)
+        templates.push(dns);
       }
     }
   }
 
   templates.sort((next, prev) => {
-    const nexts = next.split(/\s/)
-    const prevs = prev.split(/\s/)
-    return nexts[0].length < prevs[0].length ? -1 : 1
-  })
+    const nexts = next.split(/\s/);
+    const prevs = prev.split(/\s/);
+    return nexts[0].length < prevs[0].length ? -1 : 1;
+  });
 
-  return templates.join('\n')
-}
+  return templates.join("\n");
+};
 
-const nslookup = async url => {
+const nslookup = async (url) => {
   try {
-    const regex = new RegExp(`.*Name:\\s*[\\S]+\\s*Address:\\s*([0-9.]+)\\s*.*`, 'ims')
-    const dns = (await $`nslookup ${url}`).stdout
-    const ip = dns.replace(regex, '$1')
+    const regex = new RegExp(
+      `.*Name:\\s*[\\S]+\\s*Address:\\s*([0-9.]+)\\s*.*`,
+      "ims"
+    );
+    const dns = (await $`nslookup ${url}`).stdout;
+    const ip = dns.replace(regex, "$1");
 
     if (!/\d+\.\d+\.\d+\.\d+/.test(ip)) {
-      return ''
+      return "";
     }
 
-    if (ip === '127.0.0.1') {
-      return ''
+    if (ip === "127.0.0.1") {
+      return "";
     }
 
-    if (ip === '0.0.0.0') {
-      return ''
+    if (ip === "0.0.0.0") {
+      return "";
     }
 
     if (ip) {
-      return ip
+      return ip;
     }
   } catch {}
 
-  return ''
-}
+  return "";
+};
 
 writer([
-  './public/host.txt',
-  'pipelines.actions.githubusercontent.com',
-  'user-images.githubusercontent.com',
-  'favicons.githubusercontent.com',
-  'desktop.githubusercontent.com',
-  'objects.githubusercontent.com',
-  'github.global.ssl.fastly.net',
-  'media.githubusercontent.com',
-  'cloud.githubusercontent.com',
-  'camo.githubusercontent.com',
-  'raw.githubusercontent.com',
-  'github.githubassets.com',
-  'github.map.fastly.net',
-  'education.github.com',
-  'collector.github.com',
-  'codeload.github.com',
-  'central.github.com',
-  'githubstatus.com',
-  'alive.github.com',
-  'github.community',
-  'live.github.com',
-  'gist.github.com',
-  'api.github.com',
-  'github.blog',
-  'github.com',
-  'vscode.dev',
-  'github.io',
-])
+  "./public/host.txt",
+  "alive.github.com",
+  "api.github.com",
+  "assets-cdn.github.com",
+  "avatars.githubusercontent.com",
+  "avatars0.githubusercontent.com",
+  "avatars1.githubusercontent.com",
+  "avatars2.githubusercontent.com",
+  "avatars3.githubusercontent.com",
+  "avatars4.githubusercontent.com",
+  "avatars5.githubusercontent.com",
+  "camo.githubusercontent.com",
+  "central.github.com",
+  "cloud.githubusercontent.com",
+  "codeload.github.com",
+  "collector.github.com",
+  "desktop.githubusercontent.com",
+  "education.github.com",
+  "favicons.githubusercontent.com",
+  "gist.github.com",
+  "github-cloud.s3.amazonaws.com",
+  "github-com.s3.amazonaws.com",
+  "github-production-release-asset-2e65be.s3.amazonaws.com",
+  "github-production-repository-file-5c1aeb.s3.amazonaws.com",
+  "github-production-user-asset-6210df.s3.amazonaws.com",
+  "github.blog",
+  "github.com",
+  "github.community",
+  "github.githubassets.com",
+  "github.global.ssl.fastly.net",
+  "github.io",
+  "github.map.fastly.net",
+  "githubstatus.com",
+  "live.github.com",
+  "media.githubusercontent.com",
+  "objects.githubusercontent.com",
+  "pipelines.actions.githubusercontent.com",
+  "private-user-images.githubusercontent.com",
+  "raw.githubusercontent.com",
+  "user-images.githubusercontent.com",
+  "vscode.dev",
+  "particle.network",
+  "static.particle.network",
+]);
